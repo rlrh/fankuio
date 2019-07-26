63289d60d530df6a7f0e6de115900db98ff1fd0e
rlrh <rlrh1996@gmail.com>
2019-07-26 15:17:42 +0800

    complete client-side auth

---
+++ client/src/components/Landing.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Landing = () => {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1>Fankuio</h1>
+      <p>Collect feedback from your users easily.</p>
+    </div>
+  );
+};
+
+export default Landing;
